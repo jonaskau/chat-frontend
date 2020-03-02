c55abb630a87045dd969bc98f45d93f9b5117956
Jonas Kau <j.kau@energieloft.de>
2020-03-02 15:00:08 +0100

    made backendUrl depending on environment

--- src/app/backend.ts
+++ src/app/backend.ts
@@ -1,18 +1,18 @@
 import { Observable, of } from 'rxjs';
+import { environment } from '../environments/environment';
 
 export class Backend {
 
-    //private host = 'localhost:9000/';
-    private host = 'jonas-chat-api.azurewebsites.net/';
-    protected Url = `https://${this.host}`;
-    protected wsUrl = `ws://${this.host}createWSConnection`;
+    private host = environment.backendUrl
+    protected Url = `https://${this.host}`
+    protected wsUrl = `ws://${this.host}createWSConnection`
 
     constructor() {}
 
     protected handleError<T> (result? : T) {
       return (error: any): Observable<T> => {
-        console.log(error);
-        return of(result as T);
+        console.log(error)
+        return of(result as T)
       }
     }
 }
